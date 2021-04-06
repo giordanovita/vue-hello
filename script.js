a5bf0e5fc77f87f5cd80d0499e69e5e770fb133e
giordanovita <giordanovitag@libero.it>
2021-04-06 18:03:34 +0200

    esercizio completo con bonus

--- script.js
+++ script.js
@@ -7,8 +7,28 @@ BONUS: utilizzare un data come nome del file immagine in un tag img
         el: '#starter', 
         data: {
             'target': 'Vue prima stampa',
+            'pic': 'img/mountains.jpg'
         },
-       
+        methods: {
+            backSlide: function(){
+                if(this.pic=='img/mountains.jpg'){
+                    this.pic='img/mountains2.jpg'
+                }else if(this.pic=='img/mountains2.jpg'){
+                    this.pic='img/mountains1.jpg'
+                }else{
+                    this.pic='img/mountains.jpg'
+                }
+            },
+            nextSlide:function(){
+                if(this.pic=='img/mountains.jpg'){
+                    this.pic='img/mountains1.jpg'
+                }else if(this.pic=='img/mountains1.jpg'){
+                    this.pic='img/mountains2.jpg'
+                }else{
+                    this.pic='img/mountains.jpg'
+                }
+            }
+        }
     });
 }
 
